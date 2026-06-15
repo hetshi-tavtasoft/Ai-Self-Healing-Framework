b32a7bd52a4454f00521e102ab4e4d09d4b977b9
Hetshi Patel <pct113@server7.com>
2026-06-15 11:33:36 +0530

    Labeler changes

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores([
+    "dist",
+    "node_modules",
+    "playwright-report",
+    "test-results",
+    "healing-data",
+  ]),
+  {
+    files: ["**/*.ts"],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      globals: {
+        node: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+]);
